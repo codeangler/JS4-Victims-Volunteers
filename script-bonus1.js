7c6461ec8a888d902c8a642acd0ba04cb98bee8a
Casey Burnett <burnett.casey@gmail.com>
2016-05-23 17:08:15 -0600

    confirm additional victims and volunteers using confirm

--- script-bonus1.js
+++ script-bonus1.js
@@ -1,17 +1,16 @@
-// Ask user for input
-var nVictims = prompt ("How many disaster victims?");
-
-// //  Set arrays to accept victim(v) = [[Name, Phone Number, Street]]
+//  Set arrays to accept victim(v) = [[Name, Phone Number, Street]]
 var victimDetails = [];
-var i = 0;
-while (i < nVictims){
+var victim = true;
+var nVictims = 0;
+while(victim === true){
     var nName = prompt("What's the victim's name?");
     var nPhoneNum = prompt("What's the victim's phone number?");
     var nStreet = prompt("What's the victim's street name?");
  	victimDetails.push([nName, nPhoneNum,nStreet]);
-    i++;
+    nVictims ++;
+    victim = confirm("Enter another victim's information?");
+ 
 }
-
 
 // Generate a roster of victims
 var victimRoster = "";
@@ -19,19 +18,19 @@ for(var i = 0; i < nVictims; i++){
 	victimRoster = victimRoster + victimDetails[i][0] + "\n"  
 }
 
-// Ask ur a number of volunteers to record
-var numVolunteers = prompt("How many volunteers?");
+
 
 // Set array to accept volunteer details
 var volunteerDetails = [];
-
-var j = 0;
-while (j < numVolunteers){
+var volunteer = true;
+var numVolunteers = 0
+while (volunteer === true){
     var nName = prompt("What's the volunteer's name?");
     var nPhoneNum = prompt("What's the volunteers's phone number?");
     var nStreet = prompt("What's the volunteer's street name?");
  	volunteerDetails.push([nName, nPhoneNum,nStreet]);
-    j++;    
+    numVolunteers++;    
+    volunteer = confirm("Enter more volunteer details?");
 }
 
 var volunteerRoster = "";
--- script2-closure.js
+++ script2-closure.js
@@ -1,19 +1,17 @@
 // Ask user for input
-// var nVictims = prompt ("How many disaster victims?");
+var nVictims = prompt ("How many disaster victims?");
 
 // //  Set arrays to accept victim(v) = [[Name, Phone Number, Street]]
-// var victimDetails = [];
-// var i = 0;
-// while (i < nVictims){
-//     var nName = prompt("What's the victim's name?");
-//     var nPhoneNum = prompt("What's the victim's phone number?");
-//     var nStreet = prompt("What's the victim's street name?");
-//  	victimDetails.push([nName, nPhoneNum,nStreet]);
-//     i++;
-// }
+var victimDetails = [];
+var i = 0;
+while (i < nVictims){
+    var nName = prompt("What's the victim's name?");
+    var nPhoneNum = prompt("What's the victim's phone number?");
+    var nStreet = prompt("What's the victim's street name?");
+ 	victimDetails.push([nName, nPhoneNum,nStreet]);
+    i++;
+}
 
-var nVictims = 3;
-var victimDetails = [["Casey", 111, "Winter"], ["Lee", 222, "Haven"], ["Burnett", 333, "Hickok"]];
 
 // Generate a roster of victims
 var victimRoster = "";
@@ -22,21 +20,19 @@ for(var i = 0; i < nVictims; i++){
 }
 
 // Ask ur a number of volunteers to record
-// var numVolunteers = prompt("How many volunteers?");
+var numVolunteers = prompt("How many volunteers?");
 
 // Set array to accept volunteer details
-// var volunteerDetails = [];
+var volunteerDetails = [];
 
-// var j = 0;
-// while (j < numVolunteers){
-//     var nName = prompt("What's the volunteer's name?");
-//     var nPhoneNum = prompt("What's the volunteers's phone number?");
-//     var nStreet = prompt("What's the volunteer's street name?");
-//  	volunteerDetails.push([nName, nPhoneNum,nStreet]);
-//     j++;    
-// }
-var numVolunteers = 3;
-var volunteerDetails = [["Emily", 111, "Summer"], ["Lynn", 222, "Mountain"], ["Barnak", 333, "Hickok"]];
+var j = 0;
+while (j < numVolunteers){
+    var nName = prompt("What's the volunteer's name?");
+    var nPhoneNum = prompt("What's the volunteers's phone number?");
+    var nStreet = prompt("What's the volunteer's street name?");
+ 	volunteerDetails.push([nName, nPhoneNum,nStreet]);
+    j++;    
+}
 
 var volunteerRoster = "";
 for (var j = 0; j < numVolunteers; j++) {
